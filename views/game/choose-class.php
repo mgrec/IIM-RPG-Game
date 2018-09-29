111664ff5d413104ba470d98e6de13d32c042533
Grec Maxime <maxime.grec@gmail.com>
2018-09-29 18:17:03 +0200

    views : choose your class

--- views/game/choose-class.php
+++ views/game/choose-class.php
@@ -14,34 +14,53 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../../assets/css/reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="../../../assets/css/master.css">
     <title>RPG - Choose your class</title>
 </head>
 <body>
 
-<div class="section-classes">
+
+<div class="website-wrapper website-choose-class">
+
+
 
     <div class="general-about">
-
+        <p>Tu dois choisir ta classe avec précaution.</p>
     </div>
 
-    <div class="content-class">
-        <div class="classe-image"></div>
-        <p class="classe-about"></p>
-        <div class="btn-choose"></div>
+    <div class="section-classes">
+
+
+        <div class="content-class">
+            <img src="../../../assets/images/classes/bandit.png" class="classe-image">
+            <p class="classe-about">
+                Les bandits sont de véritables assassins. Ils ont une force acru, ce qui leur permettent d'eliminer leurs ennemis facilement et rapidement !
+            </p>
+            <div class="btn-choose">
+                Je suis un bandit
+            </div>
+        </div>
+
+        <div class="content-class">
+            <img src="../../../assets/images/classes/wizard.png" class="classe-image">
+            <p class="classe-about">
+                Les mages possèdent des nombreuses connaissances en terme de sorcelerie. Leurs sortilèges de soins sont les plus puissants !
+            </p>
+            <div class="btn-choose">
+                Je suis un mage
+            </div>
+        </div>
     </div>
 
-    <div>
-        <div class="classe-image"></div>
-        <p class="classe-about"></p>
-        <div class="btn-choose"></div>
+    <div class="play">
+        <form action="">
+            <input hidden type="text" value="1">
+            <input class="btn-play" type="button" value="Commencer à jouer">
+        </form>
     </div>
-
-    <form action="">
-        <input hidden type="text" value="1">
-
-    </form>
 </div>
-
 </body>
 </html>
 
